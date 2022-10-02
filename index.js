084f2517e9de5cb1ac4f184c918e0966228dec3d
Mahesh1062 <113339637+Mahesh1062@users.noreply.github.com>
2022-10-02 13:37:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const http = require("http");
 const fs = require("fs");
-fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-  });
-  fs.readFile("home.html", (err, home) => {
-    if (err) {
-      throw err;
-    }
-    http
-      .createServer((request, response) => {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(home);
-        response.end();
-      })
-      .listen(5000);
-  });
-  const http = require("http");
-const fs = require("fs");
+const args = require('minimist')(process.argv.slice(2));
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -34,7 +19,19 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
-http
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
   .createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
@@ -43,10 +40,15 @@ http
         response.write(projectContent);
         response.end();
         break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
       default:
         response.write(homeContent);
         response.end();
         break;
     }
   })
-  .listen(5000);
+  .listen(args.port);
+});
